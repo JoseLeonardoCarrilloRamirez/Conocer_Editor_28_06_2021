f54e96711acbcbcd30e657afda1df87aea38972a
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-07-07 20:57:56 -0500

    Respaso de las variables y tipos de datos 07072021

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+nombreCompletoDelProfesor = `Jose Leonardo Carrillo Ramirez`;
+miEdad = 30;
+tituloUniversitario = `Ingeniero de Sistemas`;
+altura = 1.94;
+numeroCelular = 3005558877;
+ciudad = `Bucaramanga`;
+soyUnprofesor = true;
+esFemenino = false;
+
+
+console.log(nombreCompletoDelProfesor, typeof(nombreCompletoDelProfesor));
+console.log(miEdad, typeof(miEdad));
+console.log(soyUnprofesor, typeof(soyUnprofesor));
+console.log(tituloUniversitario, typeof(tituloUniversitario));
+console.log(altura, typeof(altura));
+console.log(numeroCelular, typeof(numeroCelular));
+console.log(ciudad, typeof(ciudad));
+console.log(esFemenino, typeof(esFemenino));
